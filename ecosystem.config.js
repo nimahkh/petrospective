a10f027a49e8e51259560e47094bba3c1560f7fb
nimahabibkhoda <n.habibkhoda@trengo.com>
2022-05-09 14:50:26 +0200

    Add PM2 for deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   name: 'petrospective',
   script: 'serve',
+  instances: "max",
+  exec_mode: "cluster",
   env_production: {
+    PM2_SERVE_PORT: 8080,
     PM2_SERVE_PATH: './dist',
     PM2_SERVE_SPA: true,
     PM2_SERVE_HOMEPAGE: '/index.html'
